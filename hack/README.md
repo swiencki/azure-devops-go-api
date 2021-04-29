1713f71008261d070e9720101c4e76004ceaf8ee
swiencki <swiencki@redhat.com>
2021-04-29 16:31:58 -0700

    Updating build.go README.md

--- hack/README.md
+++ hack/README.md
@@ -1,30 +1,22 @@
-
 # Azure DevOps Golang API
-
 This repository contains Golang APIs for interacting with and managing Azure DevOps. These APIs power the Azure DevOps Extension for Azure CLI. To learn more about the Azure DevOps Extension for Azure CLI, visit the [Microsoft/azure-devops-cli-extension](https://github.com/Microsoft/azure-devops-cli-extension) repo and [azure-devops-go-api](https://github.com/Microsoft/azure-devops-go-api) repo.
-
-## Install 
-
-```
-git clone <repo>
-go mod init
-go mod tidy
-```
-
 ## Get started
-
-
 To use the API, establish a connection using a [personal access token](https://docs.microsoft.com/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts) and the URL to your Azure DevOps organization. Then get a client from the connection and make API calls.
 
+List of enviromental variables:
 ```Golang
-# Fill in with your personal access token, org URL and project
-personal_access_token = 'YOURPAT'
-organization_url = 'https://dev.azure.com/YOURORG'
-project = 'YOURPROJECT'
+    ADO_ORG='' Organization Url *string | default https://dev.azure.com/msazure/
+    DO_PROJECT='' Project Name *string | default AzureRedHatOpenShift
+    AZURE_DEVOPS_EXT_PAT='' Personal Access Token *string | default None
+    ADO_DEFID='' Comma-delimited list of Definition IDs *int | default None    
 ```
 
 ## Usage
-Run build.go
+Run build.go:
 ```bash
-$ ADO_DEFID=<Pipeline ID> go run hack/build.go
+$ [env_variable1 env_variable2] go run hack/build.go
+```
+Example:
+```bash
+$  AZURE_DEVOPS_EXT_PAT=foo ADO_DEFID=123 go run hack/build.go
 ```
--- hack/build.go
+++ hack/build.go
@@ -12,63 +12,78 @@ import (
 )
 
 func main() {
-	organizationUrl := ""
-	personalAccessToken := ""
+	// ENV variable ADO_ORG='' default https://dev.azure.com/msazure/
+	organizationUrl := getEnv("ADO_ORG", "https://dev.azure.com/msazure/")
 
-	// Create a connection to your organization
-	connection := azuredevops.NewPatConnection(organizationUrl, personalAccessToken)
-
-	ctx := context.Background()
-
-	// ENV variable ADO_PROJECT='' default AzureRedHatOpenShift
-	project := getEnv("ADO_PROJECT", "AzureRedHatOpenShift")
-
-	// ENV variable ADO_TIME='' default 60 minutes
-	userTime := getEnv("ADO_TIME", "60")
-	intUserTime, err := strconv.Atoi(userTime)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//	TODO: This should be to specify which pipeline
-	userDefID := os.Getenv("ADO_DEFID")
-	if len(userDefID) == 0 {
-		log.Fatal("A comma-delimited list of Definitions IDs required. | ADO_DEFID=''")
+	// ENV variable AZURE_DEVOPS_EXT_PAT="" default None
+	personalAccessToken := os.Getenv("AZURE_DEVOPS_EXT_PAT")
+	if len(personalAccessToken) == 0 {
+		log.Fatal("Personal Access Token required. | AZURE_DEVOPS_EXT_PAT=''")
 	} else {
-		intDefID, err := strconv.Atoi(userDefID)
-		if err != nil {
-			log.Fatal(err)
-		}
-		arIntDefID := []int{intDefID}
+		// Create a connection to your organization
+		connection := azuredevops.NewPatConnection(organizationUrl, personalAccessToken)
 
-		// Create a client to interact with build
-		buildClient, err := build.NewClient(ctx, connection)
+		ctx := context.Background()
+
+		// ENV variable ADO_PROJECT='' default AzureRedHatOpenShift
+		project := getEnv("ADO_PROJECT", "AzureRedHatOpenShift")
+
+		// ENV variable ADO_TIME='' default 60 minutes
+		userTime := getEnv("ADO_TIME", "60")
+		intUserTime, err := strconv.Atoi(userTime)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Get first page of builds for project / organization
-		buildResponseValue, err := buildClient.GetBuilds(ctx, build.GetBuildsArgs{Project: &project, MinTime: &azuredevops.Time{time.Now().Add(time.Duration(-intUserTime) * time.Minute)}, Definitions: &arIntDefID})
-		if err != nil {
-			log.Fatal(err)
-		}
-		//	log.Print(buildResponseValue)
-		for buildResponseValue != nil {
-			for _, BuildReference := range (*buildResponseValue).Value {
-				log.Print(&BuildReference.Id) //DEBUG: Prints buildId
-				buildLogsResponseValue, err := buildClient.GetBuildLogs(ctx, build.GetBuildLogsArgs{Project: &project, BuildId: BuildReference.Id})
-				if err != nil {
-					log.Fatal(err)
-				}
-				for buildLogsResponseValue != nil {
-					for _, BuildLogsReference := range *buildLogsResponseValue {
-						log.Print(&BuildLogsReference.Id)
+		// ENV variable ADO_DEFID='' default None
+		userDefID := os.Getenv("ADO_DEFID")
+		if len(userDefID) == 0 {
+			log.Fatal("A comma-delimited list of Definitions IDs required. | ADO_DEFID=''")
+		} else {
+			intDefID, err := strconv.Atoi(userDefID)
+			if err != nil {
+				log.Fatal(err)
+			}
+			arryIntDefID := []int{intDefID}
+
+			// Create a client to interact with build
+			buildClient, err := build.NewClient(ctx, connection)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Get first page of builds for organization / project / definitions within timeframe
+			buildResponse, err := buildClient.GetBuilds(ctx, build.GetBuildsArgs{Project: &project, MinTime: &azuredevops.Time{time.Now().Add(time.Duration(-intUserTime) * time.Minute)}, Definitions: &arryIntDefID})
+			if err != nil {
+				log.Fatal(err)
+			}
+			for len(buildResponse.Value) != 0 {
+				for _, BuildReference := range (*buildResponse).Value {
+					//Get first page of buildlogs from organization / project / definitions / buildid within timeframe
+					buildLogsResponse, err := buildClient.GetBuildLogs(ctx, build.GetBuildLogsArgs{Project: &project, BuildId: BuildReference.Id})
+					if err != nil {
+						log.Fatal(err)
 					}
+					for buildLogsResponse != nil {
+						for _, BuildLogsReference := range *buildLogsResponse {
+							//Get first page of loglines from organization / project / definitions / buildid / logid within timeframe
+							buildLogsLinesResponse, err := buildClient.GetBuildLogLines(ctx, build.GetBuildLogLinesArgs{Project: &project, BuildId: BuildReference.Id, LogId: BuildLogsReference.Id})
+							if err != nil {
+								log.Fatal(err)
+							}
+							for buildLogsLinesResponse != nil {
+								for _, buildLogsLineReference := range *buildLogsLinesResponse {
+									log.Print(buildLogsLineReference)
+								}
+							}
+						}
+					}
+
 				}
 			}
+			log.Print("No results returned, expand window in minutes if required. | ADO_TIME='' default 60 minutes")
 		}
 	}
-
 }
 
 func getEnv(key, fallback string) string {
